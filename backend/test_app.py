8b70a9e998845c89b2e0aac19f897410a53a86b2
Ayush Pathak <31178268+Raphaeal19@users.noreply.github.com>
2025-04-15 11:21:41 -0400

    Update test_app.py
    updating to trigger action run

--- backend/test_app.py
+++ backend/test_app.py
@@ -17,7 +17,7 @@ from app import create_app, Users
 # eliminating the possibility of carry-over from previous tests,
 # unless defined as such.
 # This fixture receives the client returned from create_app
-# in app.py. add
+# in app.py. add, delete
 @pytest.fixture
 def client():
     """
